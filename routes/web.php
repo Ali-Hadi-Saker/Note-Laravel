ce5e92ca30d3d8cc5b2f1b87cd9c5cc4b9767034
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-21 22:16:33 +0300

    added full Note routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/note', [NoteController::class, 'index'])->name('note.index');//note.index is the name of this route
+// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+// Route::post('/note/store', [NoteController::class, 'store'])->name('note.store');
+// Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+// Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+// Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+Route::resource('note', NoteController::class);
+
+
+
+
+
